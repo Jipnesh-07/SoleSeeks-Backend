a02c1836410f43bb7a8a409ec07365e86e8734ce
Manpreet Singh <97432223+Manpreet3033@users.noreply.github.com>
2024-12-24 16:36:54 +0530

    fixes

--- sockets/chat.socket.js
+++ sockets/chat.socket.js
@@ -1,42 +1,46 @@
-const Chat = require('../models/chat.model');
-const User = require('../models/user.model');
+const Chat = require("../models/chat.model");
+const User = require("../models/user.model");
 
 module.exports = (server) => {
-  const io = require('socket.io')(server, {
+  const io = require("socket.io")(server, {
     cors: {
-      origin: '*',
-      methods: ['GET', 'POST'],
+      origin: "*",
+      methods: ["GET", "POST"],
     },
   });
 
-  io.on('connection', (socket) => {
+  io.on("connection", (socket) => {
     console.log(`User connected: ${socket.id}`);
-  
+
     // Join Room
-    socket.on('join-room', (roomName) => {
+    socket.on("join-room", (roomName) => {
       if (roomName) {
         socket.join(roomName);
         console.log(`User ${socket.id} joined room: ${roomName}`);
-        io.to(roomName).emit('welcome', `Welcome to the room: ${roomName}`);
+        io.to(roomName).emit("welcome", `Welcome to the room: ${roomName}`);
       } else {
-        console.log('Room name is empty!');
+        console.log("Room name is empty!");
       }
     });
-  
+
     // Receive and Broadcast Messages
-    socket.on('message', ({ message, room }) => {
+    socket.on("message", ({ message, room, sender }) => {
       if (room) {
-        io.to(room).emit('receive-message', { sender: socket.id, message, timestamp: new Date() });
+        io.to(room).emit("receive-message", {
+          sender: sender,
+          message,
+          timestamp: new Date(),
+        });
       } else {
-        socket.emit('error', 'No room specified for the message');
+        socket.emit("error", "No room specified for the message");
       }
     });
-  
+
     // Disconnect
-    socket.on('disconnect', () => {
+    socket.on("disconnect", () => {
       console.log(`User disconnected: ${socket.id}`);
     });
   });
-  
+
   return io;
 };
